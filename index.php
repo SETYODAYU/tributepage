9637477b3c3320bd85c5f16a65f47c566228da7c
setyodayu <dayu.ikhwanika@gmail.com>
2019-06-26 10:44:06 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Barack Obama</title>
+	<style>
+		html {
+			font-size: 10px;
+		}
+
+		body {
+		  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Helvetica Neue", Arial, sans-serif;
+		  font-size: 1.6rem;
+		  line-height: 1.5;
+		  text-align: justify;
+		  color: #333;
+		  margin: 0;
+		}
+
+		h1 {
+		  font-size: 4rem;
+		  margin-bottom: 0;
+		}
+
+		h2 {
+		  font-size: 3.25rem;
+		}
+
+		a {
+		  color: #477ca7;
+		}
+
+		a:visited {
+		  color: #74638f;
+		}
+
+		main {
+		  margin: 30px 8px;
+		  padding: 15px;
+		  border-radius: 5px;
+		  background:  #0bb;
+		}
+
+
+		img {
+		  max-width: 100%;
+		  display: block;
+		  height: auto;
+		  margin: 0 auto;
+		}
+
+		#img-div {
+		  background: white;
+		  padding: 10px;
+		  margin: 0;
+		}
+
+		#img-caption {
+		  margin: 15px 0 5px 0;
+		}
+
+		#headline {
+		  margin: 50px 0;
+		  text-align: center;
+		}
+
+		ul {
+		  max-width: 550px;
+		  margin: 0 auto 50px auto;
+		  text-align: left;
+		  line-height: 1.6;
+		}
+
+		li {
+		  margin: 16px 0;
+		}
+		blockquote {
+		  font-style: italic;
+		  max-width: 545px;
+		  margin: 0 auto 50px auto;
+		  text-align: justify;
+		}
+	</style>
+</head>
+<body>
+	<main id="container">
+		<main id="header">
+			<center>
+				<h1  id="title">
+					Barack Obama
+				</h1>
+				<p><em>Presiden Amerika Serikat (2009-2017)</em></p>
+			</center>
+			<figure id="img-div">
+				<img id="image" src="https://cdn.popbela.com/content-images/post/20160804/obama-91b43a2dc55692f0b6b14c51c5fb8f50_750x500.jpg" alt="Barack Obama"  style="display: block; margin: auto; max-width: 50%; max-height: 50%">
+				<figcaption id="img-caption">
+					Barack Hussein Obama II (Dengarkani/bəˈrɑːk huːˈseɪn oʊˈbɑːmə/; lahir 4 Agustus 1961; umur 57 tahun) adalah seorang politisi Amerika yang menjabat sebagai Presiden Amerika Serikat ke-44. Ia merupakan orang Afrika Amerika pertama yang menempati jabatan tersebut.
+					<br><br>
+					Lahir di Honolulu, Hawaii, Obama merupakan lulusan Universitas Columbia dan Harvard Law School, tempat ia menjadi presiden Harvard Law Review. Ia dulunya seorang penggerak masyarakat di Chicago sebelum mendapat gelar hukumnya. Ia bekerja sebagai jaksa hak-hak sipil di Chicago dan mengajar hukum konstitusi di University of Chicago Law School sejak 1992 sampai 2004. Ia tiga kali mewakili Distrik ke-13 di Senat Illinois mulai tahun 1997 hingga 2004, namun tidak lolos ke tahap Dewan Perwakilan Rakyat Amerika Serikat tahun 2000.
+				</figcaption>
+			</figure>
+		</main>
+		<section id="pencapaian">
+			<h3 id="headline">
+				Daftar Pencapaian Prestasi Barack Obama Selama Menjabat Sebagai Presiden Amerika Serikat
+			</h3>
+			<ul>
+				<li> Presiden pertama lahir di luar 48 negara bagian yang berdekatan . [236]</li>
+				<li> Presiden pertama lahir di Hawaii . [236]</li>
+				<li> Presiden pertama yang keturunan Afrika-Amerika . [237]</li>
+				<li> Presiden pertama yang secara terbuka mendukung pernikahan sesama jenis . [238]</li>
+				<li> Presiden pertama yang memiliki Wakil Presiden Katolik ( Joe Biden ). [239]</li>
+				<li> Presiden pertama yang menunjuk mantan Ibu Negara ke Kabinet ( Hillary Clinton ). [240]</li>
+				<li> Presiden pertama yang menunjuk seorang Amerika-Latin ke Mahkamah Agung ( Sonia Sotomayor ). [241]</li>
+				<li> Presiden pertama yang menunjuk beberapa wanita ke Mahkamah Agung ( Sonia Sotomayor dan Elena Kagan ).</li>
+				<li> Presiden pertama yang menunjuk hanya perempuan ke Mahkamah Agung ( Sonia Sotomayor dan Elena Kagan ). (Catatan: Presiden Obama menominasikan Merrick Garland untuk mengisi kursi yang dikosongkan oleh Hakim Antonin Scalia , namun, konfirmasi Garland tidak pernah dibawa ke pemungutan suara)</li>
+				<li> Presiden pertama yang mengunjungi penjara federal . [242]</li>
+				<li> Presiden pertama yang menerbitkan makalah ilmiah saat di kantor. [243]</li>
+				<li> Presiden pertama yang mengunjungi Myanmar , Kamboja , Laos , Kenya , dan Ethiopia .</li>
+				<li> Presiden pertama yang memiliki potret foto resminya diambil dengan kamera digital. [244]</li>
+				<li> Presiden pertama yang memenangkan masa jabatan kedua sementara memenangkan lebih sedikit negara bagian, total suara, dan suara pemilihan daripada untuk masa jabatan pertamanya.</li>
+				<li> Presiden pertama yang menjadi pembicara utama pada Konvensi Nasional Demokrat .</li>
+				<li> Presiden pertama yang mengunjungi masjid dan sinagog saat berada di kantor.</li>
+				<li> Presiden pertama yang menyalakan diya untuk Diwali di Gedung Putih . [245]</li>
+			</ul>
+			<blockquote>
+				<p>
+					"Kita memilih harapan atas rasa takut. Kita melihat masa depan bukan sebagai sesuatu yang berada di luar kendali kita, tapi sebagai sesuatu yang dapat kita bentuk melalui upaya bersama dan kolektif"
+				</p>
+				<cite>
+					-- Barack Obama
+				</cite>
+			</blockquote>
+		</section>
+	</main>
+	<main id="tribute-link">
+		<center><p> info selanjutnya tentang Barack obama silahkan kujungi <a href="https://en.wikipedia.org/wiki/Barack_Obama">Wikipedia</a></p></center>
+	</main>
+</body>
+</html>
